3d85134658d0cec57ef33357458264fe3f4279fa
khushali.sodvadiya <khushali.sodvadiya@dbsterhub.com>
2021-06-23 17:14:44 +0530

    First commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { TrashComponent } from './components/trash/trash.component';
+
+const routes: Routes = [
+  { path: 'dashboard', component: HomeComponent },
+  { path: 'product-list', component: ProductListComponent },
+  { path: 'product-detail/:id', component: ProductDetailComponent },
+  { path: 'trash', component: TrashComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
